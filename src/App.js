f66c86c51688aafe5b29082c3342c0d37628ec06
Renan Roberto <renanroberto1@gmail.com>
2018-06-24 00:10:22 -0300

    init variable working

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import React, { Component } from 'react'
   "id": 1,
   "name": "Renan Roberto da Silva",
   "languages": ["javascript", "golang"],
+  "grades": [9.8, 9.5, 8],
   "contact": {
     "email": "renanroberto1@gmail.com",
     "tel": {
       "home": "123456789",
       "work": "987654321"
     }
-  }
+  },
+  "active": true
 }
 
 type Result struct{
@@ -26,6 +28,17 @@ type Result struct{
       Work string `json:"work"`
     } `json:"tel"`
   } `json:"contact"`
+  Active bool `json:"active"`
+}
+
+result := Result{
+  ID: 1,
+  Name: "Renan Roberto da Silva",
+  Languages: []string{"javascript", "golang"},
+  Contact: {
+    Email: "renanroberto1@gmail.com",
+  },
+  Active: true,
 }
 */
 // ---------------------------------- Example ----------------------------------
@@ -34,9 +47,31 @@ type Result struct{
 function findType(value) {
   switch(typeof value) {
     case 'number':
+      if (value % 1 !== 0) return 'float64'
       return 'int'
+    case 'boolean':
+      return 'bool'
     default:
       return typeof value
+  }
+}
+
+function typeOfValue(value) {
+  switch(typeof value) {
+    case "number":
+      return value
+    case "boolean":
+      return value
+    case "string":
+      return `"${value}"`
+    case "object":
+      if (Array.isArray(value)) {
+        const items = value.map(typeOfValue)
+        return `[]${findType(value[0])}{${items}}`
+      }
+      return ''
+    default:
+      return ''
   }
 }
 
@@ -63,25 +98,47 @@ function JSToGo(obj, level) {
   }
 
   go += '}'
-
   return go
+}
+
+function initVar(obj, level) {
+  let init = '{\n'
+
+  const tab = '\t'.repeat(level)
+
+  for (let key in obj) {
+    const value = obj[key]
+
+    if (typeof value !== 'object' || Array.isArray(value)) {
+      init += `${tab}${capitalize(key)}: ${typeOfValue(value)},\n`
+    } else {
+      init += `${tab}${capitalize(key)}: ${initVar(value, level + 1)},${tab}\n`
+    }
+  }
+
+  init += '}'
+  return init
 }
 
 function JsonToGo(json) {
   const obj = JSON.parse(json)
 
   let go = 'type Result '
+  let init = 'result := Result'
 
   go += JSToGo(obj, 1)
+  init += initVar(obj, 1)
 
-  return go
+  return { go, init }
 }
+
 // ----------------------------------- CORE ------------------------------------
 
 class App extends Component {
   state = {
     json: '',
-    go : ''
+    go : '',
+    init: ''
   }
 
   getInput = (e) => {
@@ -93,9 +150,9 @@ class App extends Component {
   sendInput = () => {
     const { json } = this.state
 
-    const go = JsonToGo(json)
+    const { go, init } = JsonToGo(json)
 
-    this.setState({ go })
+    this.setState({ go, init })
   }
 
   formatCode(code) {
@@ -112,14 +169,25 @@ class App extends Component {
   }
 
   render() {
-    const { go } = this.state
-    const code = this.formatCode(go)
+    let { go, init } = this.state
+
+    go = this.formatCode(go)
+    init = this.formatCode(init)
 
     return (
-      <div>
+      <div style={{ margin: '10px 20px' }}>
         <h1>JSON to GO</h1>
-        <textarea onChange={this.getInput}/>
-        <div>{code}</div>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div>
+            <textarea
+              cols={40}
+              rows={13}
+              onChange={this.getInput}
+            />
+          </div>
+          <div>{go}</div>
+          <div>{init}</div>
+        </div>
         <button onClick={this.sendInput}>Traduzir</button>
       </div>
     )
